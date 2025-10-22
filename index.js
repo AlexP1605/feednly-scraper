d9ce4cf61f481d600bc70337d655336396eb4563
AlexP1605 <alexandre.nageur@gmail.com>
2025-10-22 23:50:47 +0200

    Ensure polyfills load before server startup

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 import "./polyfills.js";
-import "./server.js";
+
+// Use a dynamic import so the polyfills are guaranteed to be applied
+// before the rest of the application (and its dependencies) execute.
+await import("./server.js");
